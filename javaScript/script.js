75e0f495d0ef7ce6e987fc640ec1439cca79963a
othmanjabes <k11111t12@gmail.com>
2022-06-18 16:40:30 +0300

    edit of 18-6

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,32 @@
-alert("Hi");
+var todos = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+];
+var words = [
+    "Hi",
+    "bye",
+];
+
+
+// for (let i = 0; i < todos.length; i++) {
+//     const e = todos[i];
+//     console.log(e);
+// }
+
+todos.forEach(function(i, b){
+    console.log(i, b);
+})
+
+// we create some function to do print of item
+function LogFun(todos) {
+    console.log(todos);
+}
+
+// here wo invokeing the function to print item from todos
+todos.forEach(LogFun);
+words.forEach(LogFun);
+
