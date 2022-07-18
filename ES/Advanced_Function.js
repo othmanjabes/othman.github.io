bbcc4f2dfd12039aa8cbbabfdcfdbe6a80fc998d
othmanjabes <k11111t12@gmail.com>
2022-07-18 10:24:57 +0300

    test webstorma

--- ES/Advanced_Function.js
+++ ES/Advanced_Function.js
@@ -9,9 +9,7 @@ console.log(multiplyByFive(10));
 console.log(multiplyByFive(20));
 */
 
-asd
 //Currying
 const compose = (f,g) => (a) => f(g(a));
 const sum = (num) => num + 1;
 compose(sum, sum)(5);
-a
