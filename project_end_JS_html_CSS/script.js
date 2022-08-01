a8f56c469f126536c9c51f3405a8432c45de35f8
othmanjabes <k11111t12@gmail.com>
2022-08-01 16:09:41 +0300

    in progress solving proplem

--- project_end_JS_html_CSS/script.js
+++ project_end_JS_html_CSS/script.js
@@ -1,22 +1,22 @@
-function arrayAdd(start,deleteItem,item,newArray){
-    newArray.splice(start,deleteItem,item)
-    return newArray;
-}
+// function arrayAdd(start,deleteItem,item,newArray){
+//     newArray.splice(start,deleteItem,item)
+//     return newArray;
+// }
 
-let txt = "A B C D";
-let arr = txt.split(' ');
-for (let i = 0; i < arr.length; i++) {
-    let index = arr.indexOf(arr[i]); // save the index of elemnt
-    let b = arr.findIndex(v => v === arr[i]) // save the index of elemnt
-    if (arr[i] === index) {
+// let txt = "A B C D";
+// let arr = txt.split(' ');
+// for (let i = 0; i < arr.length; i++) {
+//     let index = arr.indexOf(arr[i]); // save the index of elemnt
+//     let b = arr.findIndex(v => v === arr[i]) // save the index of elemnt
+//     if (arr[i] === index) {
         
-    }
-    console.log(index);
+//     }
+//     console.log(index);
 
-}
+// }
 
-/*
-let a = [5, 12, 8, 130, 44]
-let b = a.findIndex(v => v > 13)
-console.log(b) // 3
-*/
+// /*
+// let a = [5, 12, 8, 130, 44]
+// let b = a.findIndex(v => v > 13)
+// console.log(b) // 3
+// */
