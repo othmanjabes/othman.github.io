4e21670a7255130823008d460eba0f7d842cdeef
othmanjabes <k11111t12@gmail.com>
2022-08-03 20:45:52 +0300

    move all Exercise to privte project

---
+++ styleBG.js
@@ -0,0 +1,15 @@
+//----------
+VANTA.BIRDS({
+    el: "#body-bg",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    quantity: 2.00,
+    backgroundAlpha: 0.00
+  })
+
+  //---------
